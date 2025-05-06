603aaf65ed350bc838e8aee32c698078ba3efb03
Michał <m.majewski.dm@gmail.com>
2025-05-06 14:09:56 +0200

    added getExcursion func

--- src/js/ExcursionsAPI.js
+++ src/js/ExcursionsAPI.js
@@ -1,5 +1,17 @@
 class ExcursionsAPI {
+    constructor() {
+        this.excursionsApiUrl = 'http://localhost:3000/excursions'
+        this.ordersApiUrl = 'http://localhost:3000/orders'
+    }
 
+    getExcursions() {
+        return fetch(this.excursionsApiUrl)
+            .then(response => {
+                if (response.ok) { return response.json() }
+                return Promise.reject(response.status)
+            })
+            .catch(err => console.error('błąd pobierania wycieczki', err))
+    }
 }
 
 export default ExcursionsAPI;
